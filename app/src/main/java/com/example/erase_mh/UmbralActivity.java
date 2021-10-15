6c304bfaa3fee59e71130af000fb73e8409af2fa
Manuel Merchan <mf.merchan@uniandes.edu.co>
2021-10-15 18:07:29 -0500

    adicion de umbral y eliminacion seekbar

---
+++ app/src/main/java/com/example/erase_mh/UmbralActivity.java
@@ -0,0 +1,14 @@
+package com.example.erase_mh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UmbralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umbral);
+    }
+}
