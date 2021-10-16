35c0fbefbbe875510a822cf1d9cc88eadc6acf91
Manuel Merchan <mf.merchan@uniandes.edu.co>
2021-10-15 19:30:23 -0500

    seekbar 25-105

--- app/src/main/java/com/example/erase_mh/UmbralActivity.java
+++ app/src/main/java/com/example/erase_mh/UmbralActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class UmbralActivity extends AppCompatActivity {
+
+    private TextView umbralRango;
+    private SeekBar deslizador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_umbral);
+
+        umbralRango = findViewById(R.id.id_textViewUmbral);
+        deslizador = findViewById(R.id.seekBar);
+        deslizador.setProgress(60);
+        deslizador.setMax(80);
+
 
         Bundle datos = getIntent().getExtras();
         int scoreSection1 = datos.getInt("section1");
@@ -27,6 +38,23 @@ public class UmbralActivity extends AppCompatActivity {
                 intentScore.putExtra("section3",scoreSection3);
 
                 startActivity(intentScore);
+            }
+        });
+        deslizador.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                umbralRango.setText(String.valueOf(i+25));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
